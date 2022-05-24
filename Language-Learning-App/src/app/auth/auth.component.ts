5638d6a4389eb59d902569c6c72bb6683dd4f068
Patrycja Cader <patrycja.cader@apagroup.pl>
2022-05-24 21:33:57 +0200

    feature: authentication screen begining

---
+++ Language-Learning-App/src/app/auth/auth.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router, RouterEvent } from '@angular/router';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
+})
+export class AuthComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void { }
+}
