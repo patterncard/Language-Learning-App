fe1ac0a6f44f9130dfa710205a5e8cd69bc17ecd
Patrycja Cader <patrycja.cader@apagroup.pl>
2022-05-24 21:41:51 +0200

    fix

--- Language-Learning-App/src/app/auth/auth.module.ts
+++ Language-Learning-App/src/app/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';/
+import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
 
