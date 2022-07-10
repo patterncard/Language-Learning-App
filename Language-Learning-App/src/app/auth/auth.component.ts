9ef918e3773dfa347238e9bf5fa24234454d1ce5
Patrycja Cader <patcad@o2.pl>
2022-07-10 14:56:10 +0200

    feature: large inputs & buttons, hr

--- Language-Learning-App/src/app/auth/auth.component.ts
+++ Language-Learning-App/src/app/auth/auth.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, RouterEvent } from '@angular/router';
-
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent implements OnInit {
+  constructor() {}
 
-  constructor() {
-  }
-
-  ngOnInit(): void { }
+  ngOnInit(): void {}
 }
--- Language-Learning-App/src/app/auth/auth.module.ts
+++ Language-Learning-App/src/app/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-import {ButtonModule} from 'primeng/button';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+
 @NgModule({
   declarations: [AuthComponent],
-  imports: [CommonModule, AuthRoutingModule, ButtonModule],
+  imports: [CommonModule, AuthRoutingModule, ButtonModule, InputTextModule],
 })
 export class AuthModule {}
