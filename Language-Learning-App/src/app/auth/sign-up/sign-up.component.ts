7815ea887a951b8fcb585c0258586a2b35c0f87f
Patrycja Cader <patcad@o2.pl>
2022-09-04 10:39:16 +0200

    register api post

--- Language-Learning-App/src/app/auth/sign-up/sign-up.component.ts
+++ Language-Learning-App/src/app/auth/sign-up/sign-up.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 // import { SignUpService } from './sign-up.service';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
@@ -12,10 +13,28 @@ export class SignUpComponent {
   constructor(
     // private productService: SignUpService,
     public ref: DynamicDialogRef,
-    public config: DynamicDialogConfig
+    public config: DynamicDialogConfig,
+    private httpClient: HttpClient
   ) {}
 
   // ngOnInit() { }
 
-  showSignUp() {}
+  signUp() {
+    this.httpClient
+      .post('http://localhost:1337/api/auth/local/register', {
+        email: 'a@a.com',
+        username: 'qqwwewe',
+        password: 'asdasdasdqwe',
+      })
+      .subscribe(
+        (x) => {
+          console.log(x);
+        },
+        (qweqweqwewy) => {
+          console.log(qweqweqwewy);
+        }
+
+        
+      );
+  }
 }
