ef7ab93434ec726a2185af22032964daa92d181b
Patrycja Cader <patcad@o2.pl>
2022-09-16 13:17:02 +0200

    feature: settings

---
+++ Language-Learning-App/src/app/coins/coins.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { CoinsComponent } from './coins.component';
+
+@NgModule({
+	declarations: [CoinsComponent],
+})
+export class CoinsModule {}
---
+++ Language-Learning-App/src/app/picture/picture.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'picture',
+	templateUrl: './picture.component.html',
+	styleUrls: ['./picture.component.scss'],
+})
+export class PictureComponent {
+	constructor() {}
+}
