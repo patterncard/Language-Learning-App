2154761f3b8e68721cef378bb94c44fa94048fe2
Patrycja Cader <patcad@o2.pl>
2022-09-22 14:25:53 +0200

    feature: flashcard frontend

--- Language-Learning-App/src/app/game/flashcard/flashcard.component.ts
+++ Language-Learning-App/src/app/game/flashcard/flashcard.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
 	selector: 'flashcard',
 	templateUrl: './flashcard.component.html',
 	styleUrls: ['./flashcard.component.scss'],
 })
-export class FlashcardComponent {
-	constructor(private router: Router) {}
+export class FlashcardComponent implements OnInit {
+	constructor(private router: Router, private primengConfig: PrimeNGConfig) {}
+
+	isChecked = false;
+	isCorrectAnswear = false;
+
+	ngOnInit() {
+		this.primengConfig.ripple = true;
+		this.isCorrectAnswear = true;
+	}
 
 	goBackHome() {
-		
 		this.router.navigateByUrl('/home');
+	}
+
+	checkAnswear() {
+		this.isChecked = true;
+	}
+
+	nextFlashcard() {
+		this.isChecked = false;
 	}
 }
--- Language-Learning-App/src/app/game/flashcard/flashcard.module.ts
+++ Language-Learning-App/src/app/game/flashcard/flashcard.module.ts
@@ -1,7 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { RippleModule } from 'primeng/ripple';
 import { FlashcardComponent } from './flashcard.component';
 
 @NgModule({
 	declarations: [FlashcardComponent],
+	imports: [
+		CommonModule,
+		BrowserModule,
+		BrowserAnimationsModule,
+		InputTextModule,
+		ButtonModule,
+		RippleModule,
+	],
 })
 export class FlashcardModule {}
