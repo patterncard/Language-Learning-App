2b0417862d9e7168e31d32cf7a41e2f400ea4792
Patrycja Cader <patcad@o2.pl>
2022-11-29 21:12:44 +0100

    feature: random type of game

--- Language-Learning-App/src/app/game/game.component.ts
+++ Language-Learning-App/src/app/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { GameService } from './game.service';
 
@@ -7,8 +7,25 @@ import { GameService } from './game.service';
 	templateUrl: './game.component.html',
 	styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
 	constructor(private router: Router, private gameService: GameService) {}
+
+	ngOnInit() {
+		this.rondomizeGameType();
+	}
+
+	rondomizeGameType() {
+		const gameCount = this.gameService.gameRoutes.length;
+		const randomType = Math.floor(Math.random() * gameCount);
+		console.log(randomType);
+		if (randomType === 0) {
+			this.chooseFlashcards();
+		} else if (randomType === 1) {
+			this.chooseSentence();
+		} else {
+			this.chooseImage();
+		}
+	}
 
 	chooseFlashcards() {
 		this.gameService.selectedGameRoutes = this.gameService.gameRoutes[0];
