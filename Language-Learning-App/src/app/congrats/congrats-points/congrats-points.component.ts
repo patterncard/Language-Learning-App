6fcaacdfd9cb68f2fbdb568f7397dc81f397c898
Patrycja Cader <patcad@o2.pl>
2022-12-07 10:59:26 +0100

    fix: button color; feature: ripple

--- Language-Learning-App/src/app/congrats/congrats-points/congrats-points.component.ts
+++ Language-Learning-App/src/app/congrats/congrats-points/congrats-points.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
 	selector: 'congrats-points',
 	templateUrl: './congrats-points.component.html',
 	styleUrls: ['./congrats-points.component.scss'],
 })
-export class CongratsPointsComponent {
-	constructor() {}
+export class CongratsPointsComponent implements OnInit {
+	constructor(private primengConfig: PrimeNGConfig) {}
 
+	ngOnInit() {
+		this.primengConfig.ripple = true;
+	}
 	continue() {}
 }
--- Language-Learning-App/src/app/congrats/congrats-points/congrats-points.module.ts
+++ Language-Learning-App/src/app/congrats/congrats-points/congrats-points.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { CongratsPointsComponent } from './congrats-points.component';
 
 @NgModule({
 	declarations: [CongratsPointsComponent],
-	imports: [
-		// CommonModule, BrowserModule,
-		ButtonModule,
-		SelectButtonModule,
-	],
+	imports: [ButtonModule, SelectButtonModule, RippleModule],
 })
 export class CongratsPointsModule {}
