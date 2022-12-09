68b15afc11ede3490c9e65d2f454c74fd02f9c47
Patrycja Cader <patcad@o2.pl>
2022-12-09 11:47:42 +0100

    feature: congrats points frontend

--- Language-Learning-App/src/app/congrats/congrats-points/congrats-points.component.ts
+++ Language-Learning-App/src/app/congrats/congrats-points/congrats-points.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
+import { GameService } from 'src/app/game/game.service';
+import { AchievementsService } from 'src/app/home/achievements.service';
 
 @Component({
 	selector: 'congrats-points',
@@ -7,10 +9,17 @@ import { PrimeNGConfig } from 'primeng/api';
 	styleUrls: ['./congrats-points.component.scss'],
 })
 export class CongratsPointsComponent implements OnInit {
-	constructor(private primengConfig: PrimeNGConfig) {}
+	constructor(
+		private primengConfig: PrimeNGConfig,
+		private achievementsService: AchievementsService
+	) {}
+
+	points!: number;
 
 	ngOnInit() {
 		this.primengConfig.ripple = true;
+		// this.points = this.achievementsService.points;
+		this.points = 100;
 	}
 	continue() {}
 }
