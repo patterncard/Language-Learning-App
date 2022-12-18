c18417bd2b82f96ede3f51ca6eea030ef84d0ec8
Patrycja Cader <patcad@o2.pl>
2022-12-18 12:35:15 +0100

    feature: picture frontend

--- Language-Learning-App/src/app/picture/picture.component.ts
+++ Language-Learning-App/src/app/picture/picture.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
 	selector: 'picture',
 	templateUrl: './picture.component.html',
 	styleUrls: ['./picture.component.scss'],
 })
-export class PictureComponent {
-	constructor() {}
+export class PictureComponent implements OnInit {
+	constructor(private router: Router, private primengConfig: PrimeNGConfig) {}
+
+	ngOnInit() {
+		this.primengConfig.ripple = true;
+	}
+
+	goBackToSettings() {
+		this.router.navigateByUrl('/settings');
+	}
+
+	update() {}
 }
--- Language-Learning-App/src/app/picture/picture.module.ts
+++ Language-Learning-App/src/app/picture/picture.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 import { PictureComponent } from './picture.component';
 
 @NgModule({
 	declarations: [PictureComponent],
+	imports: [ButtonModule, RippleModule],
 })
 export class PictureModule {}
