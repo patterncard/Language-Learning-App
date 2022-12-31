5c0b19210297e79fc3d4763908f917aebb17ae76
Patrycja Cader <patcad@o2.pl>
2022-12-31 10:13:18 +0100

    feature: coins info

--- Language-Learning-App/src/app/coins/coins.component.ts
+++ Language-Learning-App/src/app/coins/coins.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'coins',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
 	styleUrls: ['./coins.component.scss'],
 })
 export class CoinsComponent {
-	constructor() {}
+	constructor(private router: Router) {}
+
+	goBackToSettings() {
+		this.router.navigateByUrl('/settings');
+	}
 }
