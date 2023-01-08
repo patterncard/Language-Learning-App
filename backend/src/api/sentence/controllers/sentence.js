e9ccc86e72648b538e1ce6105633343e499f9248
Patrycja Cader <patcad@o2.pl>
2023-01-08 12:37:40 +0100

    backend addition

---
+++ backend/src/api/sentence/controllers/sentence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  sentence controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sentence.sentence');
---
+++ backend/src/api/sentence/routes/sentence.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sentence router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::sentence.sentence');
